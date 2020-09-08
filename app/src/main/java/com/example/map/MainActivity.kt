fb7e937ccef06b912a06216524b714ad97968ede
Aiperi <aip.amankulova@gmail.com>
2020-09-08 22:46:01 +0600

    Connecting the onCreate method

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.example.map
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.map.BuildConfig.API_MAP
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.maps.Style
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MapActivity() {
+
+    override  fun getResId() = R.layout.activity_main
+    override  fun getMapView() = R.id.mapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(applicationContext, API_MAP)
-        setContentView(R.layout.activity_main)
 
-        mapView?.onCreate(savedInstanceState)
-
-        mapView?.getMapAsync {
-            it.setStyle(Style.MAPBOX_STREETS)
-
-        }
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView?.onResume()
     }
 
 }

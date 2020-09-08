3ced379a3b3a7f1922ccc061c7a3a6c2a2a38bb4
Aiperi <aip.amankulova@gmail.com>
2020-09-08 21:37:34 +0600

    Adding a map

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -2,19 +2,24 @@ package com.example.map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.map.BuildConfig.API_MAP
+import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.Style
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Mapbox.getInstance(applicationContext, API_MAP)
         setContentView(R.layout.activity_main)
 
         mapView?.onCreate(savedInstanceState)
 
         mapView?.getMapAsync {
             it.setStyle(Style.MAPBOX_STREETS)
+
         }
+
     }
 
     override fun onStart() {
@@ -25,11 +30,6 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         mapView?.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView?.onPause()
     }
 
 }
